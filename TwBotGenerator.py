f4f7511271efab067b7924ce07d6af8cd0328311
ilyavinchi <ilyavinchi@gmail.com>
2020-03-18 01:37:55 +0200

    q

--- TwBotGenerator.py
+++ TwBotGenerator.py
@@ -5,7 +5,7 @@ import sys
 import json
 import random as r
 from selenium import webdriver
-from selenium.webdriver.chrome.options importions 
+from selenium.webdriver.chrome.options import Options 
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import clipboard
